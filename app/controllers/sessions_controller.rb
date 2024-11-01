6eb67f290a788d6b5e6b8c8d5aa6e1efe975ebce
Kasvit <kasvit93@gmail.com>
2024-11-01 16:58:47 +0200

    Allow to download PDF file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,4 +6,10 @@ class SessionsController < ApplicationController
     flash[:notice] = "Logged in as #{student.full_name}"
     redirect_to root_path
   end
+
+  def logout
+    session[:student_id] = nil
+    flash[:notice] = "You have been logged out."
+    redirect_to root_path
+  end
 end
